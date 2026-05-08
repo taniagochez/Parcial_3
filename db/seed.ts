57ff91b1b32d56e353e6c227edcba70afe931ed3
luisamaya1518-rgb <luisamaya1518@gmail.com>
2026-05-07 21:44:42 -0600

    una mala ruta en seeds

--- db/seed.ts
+++ db/seed.ts
@@ -9,7 +9,7 @@ const seedAuth = betterAuth({
     schema: { user: User },
   }),
   secret: process.env.BETTER_AUTH_SECRET ?? 'mi_clave_super_secreta_12345',
-  baseURL: process.env.BETTER_AUTH_URL ?? 'https://equipo1parcial3.netlify.app',
+  baseURL: process.env.BETTER_AUTH_URL ?? 'https://parcial3equipo1uls.netlify.app',
   emailAndPassword: { enabled: true, minPasswordLength: 6 },
   user: {
     additionalFields: {
